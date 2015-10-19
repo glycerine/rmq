00edbbd02d26256ae417c727f2d01f98f0cdfb6c
Jason E. Aten <j.e.aten@gmail.com>
2015-10-18 18:15:15 -0700

    cli server bench shows corruption in Client.listeners map at client.go:304

--- src/rmq/wamp-client.go
+++ src/rmq/wamp-client.go
@@ -9,16 +9,23 @@ import (
 
 var myCallNum int64 = 0
 
+// start once; open only one socket.
+var c *turnpike.Client
+
 func client_main() {
 	turnpike.Debug()
-	c, err := turnpike.NewWebsocketClient(turnpike.JSON, "ws://localhost:8000/")
-	if err != nil {
-		log.Fatal(err)
+	var err error
+	if c == nil {
+		c, err = turnpike.NewWebsocketClient(turnpike.JSON, "ws://localhost:8000/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = c.JoinRealm("turnpike.examples", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	_, err = c.JoinRealm("turnpike.examples", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//defer c.Close()
 
 	/*
 
@@ -44,7 +51,7 @@ func client_main() {
 
 	res, err := c.Call("alarm.set", []interface{}{myCallNum}, nil)
 	if err != nil {
-		fmt.Printf("cli: error setting alarm: '%s'. call result = '%s'.\n", err, res)
+		fmt.Printf("cli: error setting alarm: '%s'. call result = '%#v'.\n", err, res)
 	} else {
 		fmt.Printf("cli: successfully called alarm.set().\n")
 	}
