e06feb23b7752db40f226c0dd1c22fbd4ade77a9
Jason E. Aten <j.e.aten@gmail.com>
2015-10-24 12:43:47 +0000

    r2r.server() and r2r.call() demos

--- R/rmq.R
+++ R/rmq.R
@@ -18,7 +18,7 @@ listenAndServe <- function(handler, addr = default.addr) {
   invisible(.Call("ListenAndServe", addr, handler, new.env(), PACKAGE="rmq"))
 }
 
-rmqcall <- function(msg, addr = default.addr) {
+rmq.call <- function(msg, addr = default.addr) {
   .Call("RmqWebsocketCall", addr, msg, PACKAGE="rmq")
 }
 
@@ -53,3 +53,64 @@ test.server <- function() {
   
   r = listenAndServe(handler, addr=default.addr)
 }
+
+## test.r2.rserver demonstrates the power of
+## the r2r.server and full serialization of R
+## objects including functions.
+##
+## This server expects to receive a message x that is assembled
+## like this:
+##
+##   x = list()
+##   x$f = <function to evaluate remotely>
+##   x$arg = <argument to give to function x$f>
+##
+## Return value: the result of doing x$f(x$arg).
+##
+test.r2r.server <- function() {
+
+  handler = function(x) {
+    print("handler called back with argument x = ")
+    print(x)
+    x$f(x$arg)
+  }
+    
+  r = r2r.server(handler, addr=default.addr)
+}
+
+r2r.server <- function(handler, addr=default.addr) {
+
+  ## If both client and server speak R's
+  ## XDR based serialization protocol
+  ## (e.g. if both ends are R sessions), then
+  ## we can serialize arbitrary R objects into
+  ## msgpack RAW bytes, transmit those RAW bytes,
+  ## and then unserialize the XDR back into full R
+  ## objects. Although not-interoperable with most other
+  ## languages, this does mean that we can exchange
+  ## *any* R object. The msgpack support for language
+  ## interop is limited to numeric arrays, string arrays,
+  ## RAW arrays, lists, and recursively nested lists.
+  ## While this level of msgpack support does cover
+  ## 99% of the inter-language use case, sometimes
+  ## we want to serialize full R objects without
+  ## restriction. For such purposes, the approach
+  ## demonstrated in the r2r.server() call and the
+  ## r2r.call() come in handy.
+  ##
+  ## Caveats: you client-server protocol can no
+  ## longer be evolved by adding new fields to the
+  ## msgpack.
+  
+  unser.handler = function(x) {
+    handler(unserialize(x, connection=NULL))
+  }
+  
+  r = listenAndServe(user.handler, addr)  
+}
+
+r2r.call <- function(msg, addr = default.addr) {
+  ## r2r.call() is the client counter-part to r2r.server().
+  rmq.call(serialize(msg), addr)
+}
+  
