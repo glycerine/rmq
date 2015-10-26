a23ce25d73f9fb6268310218bfc016ce8c689c7b
Jason E. Aten <j.e.aten@gmail.com>
2015-10-26 07:30:10 -0700

    doc++

--- R/rmq.R
+++ R/rmq.R
@@ -60,8 +60,9 @@ rmq.default.addr <- "127.0.0.1:9090"
 #' @return No return value. Blocks forever listening and calling the handler function when a request arrives. Ctrl-c will interrupt the server and shut it down. Call \code{rms.server()} again to re-start the server.
 #' @examples
 #' \dontrun{ 
-#'    ## a) the simplest echo server - in R session #1.
-#'    rmq.server(handler=function(msg) {msg}, addr="10.0.0.1:7777")
+#'    ## a) the simplest echo server - run this in R session #1. Assumes port
+#'    ##    9090 (defined in \code{rmq.default.addr}) is available locally.
+#'    rmq.server(handler=function(msg) {msg})
 #'
 #'   ## b) This second example is a simple handler
 #'   ## that echos the input it receives, and adds a few other things.
@@ -76,7 +77,7 @@ rmq.default.addr <- "127.0.0.1:9090"
 #'    reply$input = x
 #'    reply
 #'  }
-#'  r = rmq.server(handler, addr=rmq.default.addr)
+#'  r = rmq.server(handler)
 #'
 #'  ## c) lastly the client call. In R session #2. You'll
 #'  ## always need to run c) after first starting the 
