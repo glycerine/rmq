3a89760b472bb83d2eca7ca3943e21b7b4ae057a
Jason E. Aten <j.e.aten@gmail.com>
2016-04-01 21:18:41 -0700

    atg. good read.tmframe(): handles PtiOneInt64 as strings, PtiOneFloat64, PtiTwo64

--- src/rmq/readframe.go
+++ src/rmq/readframe.go
@@ -74,19 +74,45 @@ func tmFramesToR(slc []*tf.Frame) C.SEXP {
 		return C.R_NilValue
 	}
 
-	payloadList := C.allocVector(C.VECSXP, C.R_xlen_t(n))
-	C.Rf_protect(payloadList)
+	cols := 2
 
-	returnList := C.allocVector(C.VECSXP, C.R_xlen_t(2))
+	pti := slc[0].GetPTI()
+	firstPti := pti
+	var payloadList, payload2List C.SEXP
+
+	switch pti {
+	case tf.PtiOneInt64:
+		payloadList = C.allocVector(C.STRSXP, C.R_xlen_t(n))
+		C.Rf_protect(payloadList)
+	case tf.PtiOneFloat64:
+		payloadList = C.allocVector(C.REALSXP, C.R_xlen_t(n))
+		C.Rf_protect(payloadList)
+	case tf.PtiTwo64:
+		payloadList = C.allocVector(C.REALSXP, C.R_xlen_t(n))
+		C.Rf_protect(payloadList)
+		payload2List = C.allocVector(C.STRSXP, C.R_xlen_t(n))
+		C.Rf_protect(payload2List)
+		cols++
+
+	case tf.PtiUDE:
+		payloadList = C.allocVector(C.VECSXP, C.R_xlen_t(n))
+		C.Rf_protect(payloadList)
+
+	case tf.PtiZero:
+	case tf.PtiNull:
+	case tf.PtiNA:
+	case tf.PtiNaN:
+	}
+
+	returnList := C.allocVector(C.VECSXP, C.R_xlen_t(cols))
 	C.Rf_protect(returnList)
 
-	var sxpTy C.SEXPTYPE = C.REALSXP
-	numSlice := C.allocVector(sxpTy, C.R_xlen_t(n))
-	C.Rf_protect(numSlice)
+	timestampSlice := C.allocVector(C.REALSXP, C.R_xlen_t(n))
+	C.Rf_protect(timestampSlice)
 	size := unsafe.Sizeof(C.double(0))
 
 	var rhs C.double
-	ptrNumSlice := unsafe.Pointer(C.REAL(numSlice))
+	ptrNumSlice := unsafe.Pointer(C.REAL(timestampSlice))
 	const msec = 1e6
 	for i, f := range slc {
 		// timestamp
@@ -97,20 +123,59 @@ func tmFramesToR(slc []*tf.Frame) C.SEXP {
 		*((*C.double)(unsafe.Pointer(uintptr(ptrNumSlice) + size*uintptr(i)))) = rhs
 
 		// payload
-		evtnum := f.GetEvtnum()
-		if evtnum == tf.EvJson || (evtnum >= 2000 && evtnum <= 9999) {
-			C.SET_VECTOR_ELT(payloadList, C.R_xlen_t(i), decodeJsonToR(f.Data))
-
-		} else if evtnum == tf.EvMsgpKafka || evtnum == tf.EvMsgpack {
-			C.SET_VECTOR_ELT(payloadList, C.R_xlen_t(i), decodeMsgpackToR(f.Data))
+		pti = f.GetPTI()
+		if pti != firstPti {
+			panic(fmt.Sprintf("inconsistent pti, firstPti was '%v', now we have '%v'",
+				firstPti, pti))
 		}
+		switch pti {
+		case tf.PtiOneInt64:
+			C.SET_STRING_ELT(payloadList, C.R_xlen_t(i), C.mkChar(C.CString(fmt.Sprintf("%d", f.Ude))))
+		case tf.PtiOneFloat64:
+			rhs = C.double(f.V0)
+			ptrPayList := unsafe.Pointer(C.REAL(payloadList))
+			*((*C.double)(unsafe.Pointer(uintptr(ptrPayList) + size*uintptr(i)))) = rhs
+
+		case tf.PtiTwo64:
+			rhs = C.double(f.V0)
+			ptrPayList := unsafe.Pointer(C.REAL(payloadList))
+			*((*C.double)(unsafe.Pointer(uintptr(ptrPayList) + size*uintptr(i)))) = rhs
+			C.SET_STRING_ELT(payload2List, C.R_xlen_t(i), C.mkChar(C.CString(fmt.Sprintf("%d", f.Ude))))
+
+		case tf.PtiUDE:
+
+			// probably json or msgpack, try to decode it.
+			evtnum := f.GetEvtnum()
+			if evtnum == tf.EvJson || (evtnum >= 2000 && evtnum <= 9999) {
+				tmp := decodeJsonToR(f.Data)
+				C.Rf_protect(tmp)
+				C.SET_VECTOR_ELT(payloadList, C.R_xlen_t(i), tmp)
+				C.Rf_unprotect_ptr(tmp)
+			} else if evtnum == tf.EvMsgpKafka || evtnum == tf.EvMsgpack {
+				tmp := decodeMsgpackToR(f.Data)
+				C.Rf_protect(tmp)
+				C.SET_VECTOR_ELT(payloadList, C.R_xlen_t(i), tmp)
+				C.Rf_unprotect_ptr(tmp)
+			}
+
+		case tf.PtiZero:
+		case tf.PtiNull:
+		case tf.PtiNA:
+		case tf.PtiNaN:
+		}
+
+	} // end for range slc
+
+	C.SET_VECTOR_ELT(returnList, C.R_xlen_t(0), timestampSlice)
+	C.SET_VECTOR_ELT(returnList, C.R_xlen_t(1), payloadList)
+	if cols == 3 {
+		C.SET_VECTOR_ELT(returnList, C.R_xlen_t(2), payload2List)
+		C.Rf_unprotect_ptr(payload2List)
 	}
 
-	C.SET_VECTOR_ELT(returnList, C.R_xlen_t(0), numSlice)
-	C.SET_VECTOR_ELT(returnList, C.R_xlen_t(1), payloadList)
-
-	C.Rf_unprotect_ptr(numSlice)
-	C.Rf_unprotect_ptr(returnList)
+	C.Rf_unprotect_ptr(timestampSlice)
 	C.Rf_unprotect_ptr(payloadList)
+
+	C.Rf_unprotect_ptr(returnList)
 	return returnList
 }
